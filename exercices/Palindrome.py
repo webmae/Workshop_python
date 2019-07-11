3dbda151d94fc16a0220b862f4a0ed70ee313d75
nowakperry <nowakperrykeanou@gmail.com>
2019-07-11 16:48:45 +0200

    correction des énoncés des exercices

--- exercices/Palindrome.py
+++ exercices/Palindrome.py
@@ -3,8 +3,6 @@
 """
 Exercice numéro 1 : Palindrome.
 Crée une fonction qui affiche si la chaîne de caratère est un palindrome ou non.
-crée une liste de chaque caractère de la chaîne
-fait un copie de cette liste et reverse la
-compare le premier élément de la premiere liste au dernier element de la copie
+
 """
 
--- exercices/jeux_plus_ou_moins.py
+++ exercices/jeux_plus_ou_moins.py
@@ -5,6 +5,8 @@ Jeux du plus ou moins
 
 Fais en sorte que le programme choisisse un nombre au hasard entre 0 et 1000 inclus 
 et trouves-le en 8 chances.
+lorsque que tu entres un nombre, fais en sorte que le programme te dise si le nombre à trouvé 
+est plus petit ou plus grand
 Si tu ne l'as pas trouvé,retourne un game over 
 """
 
