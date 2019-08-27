7bd9291b3e58bf72cb86556145162346a7f5caeb
webmae <emily.marghella@gmail.com>
2019-08-27 11:41:26 +0200

    jeux dichotomie

--- exercices/jeux_plus_ou_moins.py
+++ exercices/jeux_plus_ou_moins.py
@@ -5,8 +5,38 @@ Jeux du plus ou moins
 
 Fais en sorte que le programme choisisse un nombre au hasard entre 0 et 1000 inclus 
 et trouves-le en 8 chances.
-lorsque que tu entres un nombre, fais en sorte que le programme te dise si le nombre à trouvé 
+lorsque que tu entres un nombre, fais en sorte que le programme te dise si le nombre à trouver 
 est plus petit ou plus grand
-Si tu ne l'as pas trouvé,retourne un game over 
+Si tu ne l'as pas trouvé, retourne un game over 
+
+print(value)
+    if tresor < value:
+        print("Nope.)
+    elif tresor > value:
+        print("Nope.)
+    else:
+        print("Congratulation! Computer: ".tresor.". You: ". value)
+
+
 """
 
+from random import randrange
+tresor = randrange(800)
+tresor = int(tresor)
+user = 8
+user = int(user)
+value = 0
+while (user > 0) and (value != tresor):
+    value = input("Guess a number between 0 and 800. You have "+format(user)+" chances to find the answer!")
+    value = int(value)
+    print(value)
+    if tresor < value:
+        user = user-1
+        print("Nope. It's smaller than "+format(value)+". But you still have "+format(user)+" chances!")
+    elif tresor > value:
+        user = user-1
+        print("Nope. It's higher than "+format(value)+". But you still have "+format(user)+" chances!")   
+    else:
+        print("Congratulation! Computer: "+format(tresor)+". You: "+format(value))
+if user == 0:
+    print("Sorry, you lost. The answer was: "+format(tresor))
